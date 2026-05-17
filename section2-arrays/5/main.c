ae1f7c6ba2f9446538f0b2c89bde917756287c3f
lapeko <lapeko88@gmail.com>
2026-05-17 23:55:32 +0200

    initialize struct without using properties

--- section2-arrays/5/main.c
+++ section2-arrays/5/main.c
@@ -6,14 +6,14 @@ struct Rectangle {
 };
 
 int main() {
-    struct Rectangle r = {width: 4, height: 5};
+    struct Rectangle r = {.width = 4, .height = 5};
 
     int area = r.width * r.height;
 
     printf("area is: %d\n", area);
     printf("size of struct r is: %ld\n", sizeof(r));
 
-    struct Rectangle rs[2] = {{.width = 4, .height = 5}};
+    struct Rectangle rs[2] = {{4, 5}};
 
     printf("rs[1] width: %d, length: %d\n", rs[1].width, rs[1].height);
 
